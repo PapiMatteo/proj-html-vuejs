6b6ddcf7a3f1dbe2a0b6d5c86585791f65af50c4
PapiMatteo <matteopapi.work@gmail.com>
2023-12-04 13:58:16 +0100

    add: header components implemented

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      headerNav: [
+        'Home',
+        'Order Online',
+        'About',
+        'News',
+        'Contact Us'
+      ]
+    }
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <div class="header-section">
+    <AppHeader :navItems="headerNav"/>
+  </div>
+  
+</template>
 
 <style lang="scss">
 @use './style/general.scss';
+@import '@fortawesome/fontawesome-free/css/all.css';
+
+.header-section {
+  background-image: url('./assets/img/home-background-hero-scaled.jpg');
+  background-size: cover;
+  min-height: 850px;
+}
+
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,49 @@
 <script>
 export default {
-
+    props: {
+        navItems: Array
+    }
 }
 </script>
 
 <template>
+    <header>
+        <div class="container">
 
+            <div class="header-content">
+                <!-- Logo -->
+                <a href=""><img src="../assets/img/takeout-logo-2x-200x43.png" alt=""></a>
+                <ul>
+                    <li v-for="item in navItems"><a href="">{{ item }}</a></li>
+                    <li><a href=""><i class="fa-solid fa-cart-shopping"></i></a></li>
+                </ul>
+            </div>
+            
+        </div>
+    </header>
+    
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    width: 70%;
+    margin: 0 auto;
+
+    .header-content {
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    a {
+        text-decoration: none;
+        color: white;
+    }
+    ul {
+        list-style-type: none;
+        display: flex;
+        gap: 2rem;
+    }
+}
+
+</style>
